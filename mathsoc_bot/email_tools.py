2ee68963410b4d6dcb9c03b65c8b313c19089c8a
Ben Simms <ben@bensimms.moe>
2020-09-25 18:37:12 +0100

    Remove sendgrid

--- mathsoc_bot/email_tools.py
+++ mathsoc_bot/email_tools.py
@@ -1,10 +1,11 @@
 import logging
 import textwrap
-import aiohttp
+from aiosmtplib import SMTP
+from email.message import EmailMessage
 
 from discord.ext.commands import BadArgument
 
-from mathsoc_bot.secrets import sendgrid_token
+from mathsoc_bot.constants import from_email_address
 
 logger = logging.getLogger(__name__)
 
@@ -25,21 +26,16 @@ async def send_verify_email(target_email: str, token: str):
         """
     )
 
-    async with aiohttp.ClientSession() as sess:
-        async with sess.post(
-                "https://api.sendgrid.com/v3/mail/send",
-                headers={
-                    "Authorization": f"Bearer {sendgrid_token}",
-                },
-                json={
-                    "personalizations": [{"to": [{"email": target_email}],
-                                          "subject": subject}],
-                    "content": [{"type": "text/plain", "value": body}],
-                    "from": {"email": "verifier@luhack.me", "name": "Mathsoc Verification"},
+    message = EmailMessage()
+    message["From"] = from_email_address
+    message["To"] = target_email
+    message["Subject"] = subject
+    message.set_content(body)
 
-                }
-        ) as r:
-            assert r.status == 202, await r.read()
+    smtp_client = SMTP(hostname="smtp.lancs.ac.uk")
+    async with smtp_client:
+        await smtp_client.send_message(message)
+
 
     logger.info(f"Sent auth email to: {target_email}")
 
--- mathsoc_bot/secrets.py
+++ mathsoc_bot/secrets.py
@@ -11,8 +11,6 @@ def env_fail(var: str):
     exit(1)
 
 
-sendgrid_token = os.getenv("SENDGRID_TOKEN") or env_fail("SENDGRID_TOKEN")
-
 bot_client_token = os.getenv("BOT_TOKEN") or env_fail("BOT_TOKEN")
 
 signing_secret = os.getenv("TOKEN_SECRET") or env_fail("TOKEN_SECRET")
